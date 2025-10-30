39d4db66585da7aaa6047ffa5f383b2136bd1557
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-10-30 17:02:35 +0300

    Add postgresql grammar

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+CURRENT_DIR := $(shell pwd)
+CODEGEN_DIR := $(CURRENT_DIR)/src/stewkk/sql/logic/parser/codegen
+PARSER_SOURCE_DIR := $(CURRENT_DIR)/src/stewkk/sql/logic/parser
+
+build:
+	cmake --build build -- -j 6
 
 codegen:
-	@cd docs && antlr -Dlanguage=Cpp -visitor -o ../src/stewkk/sql/codegen -package stewkk::sql::codegen TParser.g4 TLexer.g4
+	@antlr -Dlanguage=Cpp -visitor -o $(CODEGEN_DIR) -package stewkk::sql::codegen $(PARSER_SOURCE_DIR)/PostgreSQLParser.g4 $(PARSER_SOURCE_DIR)/PostgreSQLLexer.g4
 
-.PHONY: codegen
+.PHONY: codegen build
--- src/stewkk/sql/main.cpp
+++ src/stewkk/sql/main.cpp
@@ -2,38 +2,15 @@
 
 #include <antlr4-runtime.h>
 
-#include <stewkk/sql/codegen/TLexer.h>
-#include <stewkk/sql/codegen/TParser.h>
-#include <stewkk/sql/codegen/TParserBaseVisitor.h>
-
-#include <stewkk/sql/logic/llvm_ir_builder.hpp>
+#include <stewkk/sql/logic/parser/codegen/PostgreSQLLexer.h>
+#include <stewkk/sql/logic/parser/codegen/PostgreSQLParser.h>
+#include <stewkk/sql/logic/parser/codegen/PostgreSQLParserBaseVisitor.h>
 
 using namespace stewkk::sql::codegen;
-using namespace stewkk::sql::logic;
 using namespace antlr4;
 
 int main() {
-  std::ifstream f("example.txt");
-
-  ANTLRInputStream input(f);
-  TLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
-
-  tokens.fill();
-  for (auto token : tokens.getTokens()) {
-    std::cout << token->toString() << std::endl;
-  }
-
-  TParser parser(&tokens);
-  tree::ParseTree* tree = parser.main();
-
-  std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
-
-  Visitor visitor;
-  visitor.visit(tree);
-
-  auto ir = visitor.GetIr();
-  ir->print(llvm::errs(), nullptr);
+  std::cout << "Hello\n";
 
   return 0;
 }
